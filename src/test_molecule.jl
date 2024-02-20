b012314552fb24345bd981c2473228591a7c8376
Jonas Pueschel <pueschel@barcelona.math.uni-augsburg.de>
2024-02-20 11:45:17 +0100

    minor changes

--- src/test_molecule.jl
+++ src/test_molecule.jl
@@ -5,7 +5,7 @@ using LinearAlgebra
 include("./rcg.jl")
 include("setups/all_setups.jl")
 
-model, basis = silicon_setup(Ecut = 50);
+model, basis = graphene_setup(Ecut = 100);
 
 # Convergence we desire in the density
 tol = 1e-6
@@ -27,7 +27,7 @@ scfres_start = self_consistent_field(basis; ψ = ψ0 , maxiter = 1);
 options1 = default_options(basis, ψ1)
 options1.gradient_options.gradient = "H1"
 options1.retraction = "polar"
-options1.β_cg = "HS"
+options1.β_cg = "FR-PRP"
 options1.step_size_options.bt_iter = 10;
 options1.step_size_options.step_size = "aH"
 
@@ -36,17 +36,17 @@ options1.step_size_options.step_size = "aH"
 end
 #heatmap(scfres_rcg1.ρ[:, :, 1, 1], c=:blues)
 
-options2 = default_options(basis, ψ1)
-options2.gradient_options.gradient = "ea"
-options2.gradient_options.inner_iter = 100
-options2.retraction = "polar"
-options2.β_cg = "FR-PRP"
-options2.step_size_options.bt_iter = 10;
-options2.step_size_options.step_size = "eH"
+#options2 = default_options(basis, ψ1)
+#options2.gradient_options.gradient = "ea"
+#options2.gradient_options.inner_iter = 100
+#options2.retraction = "polar"
+#options2.β_cg = "FR-PRP"
+#options2.step_size_options.bt_iter = 10;
+#options2.step_size_options.step_size = "eH"
 
-@time begin   
-    scfres_rcg1 = rcg(basis, ψ1; tol = tol, maxiter = 200, options = options2);print("");
-end
+#@time begin   
+#    scfres_rcg1 = rcg(basis, ψ1; tol = tol, maxiter = 200, options = options2);print("");
+#end
 
 @time begin
     scfres_scf = self_consistent_field(basis; tol, ψ = ψ1);print("");
