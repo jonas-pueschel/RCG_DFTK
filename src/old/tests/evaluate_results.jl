09dba6ab58a0d373a14a4c01ff776a1146dbf80e
Jonas Pueschel <pueschel@barcelona.math.uni-augsburg.de>
2024-09-05 13:43:59 +0200

    removed unwanted files

--- src/old/tests/evaluate_results.jl
+++
@@ -1,9 +0,0 @@
-using Plots
-
-result_path = "./src/tests/results/"
-result_prefix = "fullres_"
-partresult_prefix = "partialres_"
-
-molecule = "graphene"
-#"graphene", "H2", "silicon", "GaAs", "TiO2"
-methods_to_test = ["dcm_lbfgs", "rcg_h1_greedy", "rcg_inea_shift_greedy", "rcg_h1", "rcg_h1_ah_greedy", "rcg_ea_shift", "dcm_cg", "rcg_h1_ah", "scf_naive", "rcg_ea", "rcg_inea_shift", "rcg_ea_ah_shift", "scf"]
